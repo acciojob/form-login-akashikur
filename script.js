9bf049555bcd5262ecb57689ff4bd5d134be1b62
Akash M <108210779+akashikur@users.noreply.github.com>
2023-07-01 14:50:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	const firstName = document.getElementById('firstName').value;
+  const lastName = document.getElementById('lastName').value;
+
+  // Check if the first and last name fields are not empty
+  if (firstName && lastName) {
+    alert(`${firstName} ${lastName}`);
+  } 
 
 }
